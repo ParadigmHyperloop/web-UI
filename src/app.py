6fc4e731e5c2b705d9444090777cb984c640fd39
Eddie Hurtig <eddie@hurtigtechnologies.com>
2018-01-09 22:19:42 -0500

    Add requirements.txt

--- src/app.py
+++ src/app.py
@@ -1,93 +1,75 @@
 #!/usr/bin/env python
-
 import os
+from datetime import datetime
 from flask import Flask, g, jsonify, current_app, request, \
-                  send_from_directory, render_template, redirect
+                  send_from_directory, render_template, redirect, abort
+
 
 app = Flask(__name__)
-_ods = None
-_pod = None
-
-WEB_ROOT = os.environ.get('WEB_ROOT', '.')
-DATA_SERVICES = os.path.realpath(os.path.join(__file__, '../../data_services'))
 
 
-def get_ods():
-    """Returns the ODS server"""
-    return _ods
+NAV_BAR = {
+  'index': {
+    'href': 'index.html',
+    'title': 'Vehicle Dashboard',
+    'icon': 'pe-7s-graph'
+  },
+  'profile': {
+    'href': 'profile.html',
+    'title': 'Flight Profile',
+    'icon': 'pe-7s-user'
+  },
+  'overrides': {
+    'href': 'overrides.html',
+    'title': 'Sensor Overrides',
+    'icon': 'pe-7s-note2'
+  },
+  'manual': {
+    'href': 'manual.html',
+    'title': 'Manual Control',
+    'icon': 'pe-7s-news-paper'
+  },
+  'feeds': {
+    'href': 'feeds.html',
+    'title': 'Live Streams',
+    'icon': 'pe-7s-science'
+  },
+  'health': {
+    'href': 'health.html',
+    'title': 'Vehicle Health',
+    'icon': 'pe-7s-map-marker'
+  },
+  'notifications': {
+    'href': 'notifications.html',
+    'title': 'Notifications',
+    'icon': 'pe-7s-bell'
+  },
+}
 
 
-def set_ods(ods_server):
-    """Tells flask where the ODS Server class is"""
-    global _ods
-    _ods = ods_server
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow(),
+            'navigation_bar': NAV_BAR}
 
 
-def get_pod():
-    """Returns the ORCS Server class"""
-    return _pod
-
-
-def set_pod(pod):
-    """Tells flask where the ORCS Server class is"""
-    global _pod
-    _pod = pod
-
-
-def send_path(path):
-    if os.path.isdir(path):
-        path = os.path.join(path, 'index.html')
-
-    if os.path.isfile(os.path.join("./templates", os.path.basename(path))):
-        return render_template(os.path.basename(path))
-
-    if os.path.exists(path):
-        return send_from_directory(os.path.dirname(path),
-                                   os.path.basename(path))
-    else:
-        return jsonify({"msg": "{} does not exist".format(path)})
-
-
-@app.route("/ui/", defaults={'path': ''})
+@app.route("/ui/", defaults={'path': 'index'})
 @app.route("/ui/<path:path>")
 def ui(path):
-    path = os.path.normpath(os.path.join(os.getcwd(), WEB_ROOT, path))
-    return send_path(path)
+    page = path.split('.')[0]
+    return render_template(path,
+                           active_page=page,
+                           title=NAV_BAR[page]['title'])
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', title="Page Not Found"), 404
 
 
 @app.route("/")
 def index():
     return redirect('/ui/')
-
-
-@app.route("/state")
-def state():
-    return jsonify(get_ods().get_state())
-
-
-@app.route("/sensors")
-def sensors():
-    path = os.path.join(DATA_SERVICES, "sensors.json")
-    return send_path(path)
-
-
-@app.route("/commands/<command>", methods=['POST'])
-def command(command):
-    argv = [command]
-
-    data = request.get_json()
-    if data and 'args' in data:
-        argv.extend(data['args'])
-
-    data = None
-    try:
-        data = get_pod().run(' '.join(argv))
-    finally:
-        return jsonify({
-            "status": "failed" if data is None else "ok",
-            "argv": argv,
-            "msg": data
-        })
 
 
 if __name__ == '__main__':
